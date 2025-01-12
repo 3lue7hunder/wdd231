2ea6b2bd06eed58a1c44090c81cb5080eaa39058
3lue7hunder <tyrusnell@gmail.com>
2025-01-11 21:46:25 -0700

    Course Home Page

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+// Get the current year
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Get the last modified date of the document
+document.getElementById('lastModified').textContent = `Last modified: ${document.lastModified}`;
