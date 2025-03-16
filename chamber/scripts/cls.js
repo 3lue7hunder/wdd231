5ba0473306e1c8f1eff248ff79f769b704a10743
3lue7hunder <tyrusnell@gmail.com>
2025-03-15 18:33:47 -0600

    remove chamber file

--- chamber/scripts/cls.js
+++
@@ -1,5 +0,0 @@
-new PerformanceObserver((entryList) => {
-    for (const entry of entryList.getEntries()) {
-      console.log('Layout shift:', entry);
-    }
-  }).observe({type: 'layout-shift', buffered: true});
--- chamber/scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('#lastModified').textContent = "Last Modification: "+document.lastModified;
-
-const rightNow = new Date();
-console.log(rightNow);
-console.log(rightNow.getFullYear());
-document.querySelector('#currentyear').textContent = rightNow.getFullYear();
-
