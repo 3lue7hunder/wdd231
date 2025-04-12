fcae6b2c6f0863dfed9d9be21f543f1534d2eb6a
3lue7hunder <tyrusnell@gmail.com>
2025-04-12 16:23:29 -0600

    Added lazy loading for images

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -12,6 +12,7 @@ function displayItems(places) {
         const thephoto = document.createElement('img');
         thephoto.src = `images/${x.photo_link}`;
         thephoto.alt = x.name;
+        thephoto.loading = "lazy";
         thecard.appendChild(thephoto);
 
         const thetitle = document.createElement('h2');
